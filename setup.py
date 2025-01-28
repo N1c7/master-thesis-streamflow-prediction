ef6c40e4ca04f3c6a9eee38a0671e51e4e1dcfa1
N1c7 <nick.vannuland99@gmail.com>
2025-01-28 21:43:17 +0100

    minor changes

--- setup.py
+++ setup.py
@@ -9,24 +9,24 @@ setup(
     py_modules=['project_paths'],
     install_requires=[
         # Core data science packages
-        'numpy>=1.19.0',
-        'pandas>=1.0.0',
-        'scipy>=1.5.0',
-        'xarray>=0.16.0',
-        'geopandas>=0.10.0',
-        'shapely>=1.8.0',
+        'numpy==1.24.4',
+        'pandas==1.5.3',
+        'scipy==1.10.1',
+        'xarray==2023.1.0',
+        'geopandas==0.13.2',
+        'shapely==2.0.6',
         
         # Machine Learning packages
-        'scikit-learn>=0.23.0',
-        'tensorflow==2.12.0',
-        'keras-tuner>=1.1.0',
-        'scikit-optimize>=0.8.1',
+        'scikit-learn==1.3.2',
+        'tensorflow==2.9.0',
+        'keras-tuner==1.4.7',
+        'scikit-optimize==0.10.2',
         
         # Utilities
-        'joblib>=0.16.0',
+        'joblib==1.4.2',
         
         # Visualization
-        'matplotlib>=3.2.0',
+        'matplotlib==3.7.5',
     ],
-    python_requires='>=3.8,<3.9'
+    python_requires='==3.8.0'
 )
